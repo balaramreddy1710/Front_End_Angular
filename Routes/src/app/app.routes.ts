62e9852e6e04814a7964ff40b5ffc09d725987d0
balaram <balaramreddy1710@gmail.com>
2025-05-29 16:55:34 +0530

    [Balaram] Add: Routing and Navigation in angular

---
+++ Routes/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about/:id', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
