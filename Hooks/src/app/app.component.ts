f43a7193a4e64ebef139c6940a4d8f7ea1c088a0
balaram <balaramreddy1710@gmail.com>
2025-05-29 15:03:57 +0530

    [Balaram] Add: Usage of ngOnInit and ngDoChanges hooks in angular

--- Hooks/src/app/app.component.ts
+++ Hooks/src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChangeComponent } from './change/change.component';
 import { FormsModule } from '@angular/forms';
+import { AfterComponent } from './after/after.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChangeComponent, FormsModule],
+  imports: [RouterOutlet, ChangeComponent, FormsModule, AfterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
   itemList = [
     {
       id: 1,
@@ -34,4 +35,19 @@ export class AppComponent {
   ];
 
   title = 'Hello';
+
+  message = '';
+
+  data = 'First';
+  prevData = 'First';
+  ngOnInit(): void {
+    this.message = 'This is the value';
+  }
+
+  ngDoCheck(): void {
+    if (this.data !== this.prevData) {
+      console.log('Data changed', this.data);
+      this.prevData = this.data;
+    }
+  }
 }
